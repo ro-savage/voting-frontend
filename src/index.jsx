97d307fe79fbc01a789722556d26cfe670b101ea
Ro Savage <rowan.savage@gmail.com>
2016-03-08 17:03:18 +1300

    Add ability to pass callback and check for previous voting to vote button

--- src/index.jsx
+++ src/index.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Voting from './components/Voting'
 
-const pair = ['Trainspotting', '28 Days Later']
+const pair = ['Export', 'Import']
 
 ReactDOM.render(
-  <Voting pair={pair} />,
+  <Voting pair={pair} vote={()=> {console.log('OMG')}} hasVoted="Export" />,
     document.getElementById('app')
 )
